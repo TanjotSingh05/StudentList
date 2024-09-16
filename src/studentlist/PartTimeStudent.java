718f86c370625a5d9e6801d40d2ae81aa9a1fee1
tanjotsingh <tanjotsingh@10.48.43.165>
2024-09-16 10:38:15 -0400

    part time student file addded

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author tanjotsingh
+ */
+public class PartTimeStudent extends Student{
+    
+    private int NumCourses;
+
+    public int getNumCourses() {
+        return NumCourses;
+    }
+
+    public void setNumCourses(int NumCourses) {
+        this.NumCourses = NumCourses;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
