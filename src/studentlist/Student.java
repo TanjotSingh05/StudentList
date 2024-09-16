21ecd592acf297d86229f397a54f3164323b8416
tanjotsingh <tanjotsingh@10.48.43.165>
2024-09-16 09:38:48 -0400

    Revert "Student.java updated"
    This reverts commit 38b5196910e18c94bd8e301997cfeefe2889bffa.

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -10,7 +10,7 @@ package studentlist;
  */
 public class Student {
     private String name;
-    private String studentId;
+    private String studentID;
     private String address;
 
     public String getName() {
